2ec513e5bcd380dc820ac6504b2a721f2a0b3b61
sunhao_win <sunhao@zongheng.com>
2017-03-29 14:51:27 +0800

    0329-恢复之前的并进行修改提交

--- js/chart.js
+++ js/chart.js
@@ -13,175 +13,8 @@ $(function (){
             'echarts/chart/line'
         ],
         function (ec) {
-            //var myChart = ec.init(document.getElementById('tjmap'));
-            var barOption={
-                tooltip : {
-                    trigger: 'axis',
-                    formatter: "{b}<br/>{a}: {c}棵"
-                },
-                xAxis : [
-                    {
-                        name:"行政区域",
-                        nameTextStyle:{"fontSize":"14","color":"#808080"},
-                        data : ['城北街道','城南街道','南口','沙河','南邵','崔村','阳坊','流村','马池口','十三陵','长陵','北七家'],                        
-                        axisLabel:{
-                            textStyle: {
-                                color: '#808080'
-                            }
-                        },
-                        axisLine:{
-                            lineStyle:{
-                                color: '#969696',
-                                width: 1,
-                                type: 'solid'
-                            }  
-                        },
-                        axisTick:{show:false}
-                    }
-                ],
-                yAxis : [
-                    {
-                        name : '树木(棵)',
-                        nameTextStyle:{"fontSize":"14","color":"#808080"},
-                        axisLabel:{
-                            textStyle: {
-                                color: '#808080'
-                            }
-                        },
-                        axisLine:{
-                            lineStyle:{
-                                color: '#969696',
-                                width: 1,
-                                type: 'solid'
-                            }  
-                        }
-                    }
-                ],
-                series : [
-                    {
-                        name:'树木总数',
-                        type:'bar',
-                        barWidth:20, 
-                        itemStyle: { 
-                            normal: {
-                                color: function(params) {
-                                    var colorList = ['#91a86f','#7fa86f','#6fa870','#6fa88d','#6fa8a6','#7c9fb2','#8699b9','#a0a2c8','#b9a0c8','#ca9dba','#d4a9ac','#d5c096','#c5cc8c','#9ebc80','#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83','#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'];
-                                    return colorList[params.dataIndex]
-                                },
-
-                                label : {
-                                    show: true, 
-                                    position: 'top',
-                                    textStyle: {
-                                        color: '#808080'
-                                    }
-                                }
-                            }
-                        },
-                        data:[20,100,123,400,90,70,230,600,500,400,200,300,445]
-                    }
-                ]
-
-            };
-            var lineOption={
-                tooltip : {
-                    trigger: 'axis',
-                    formatter: "{b}<br/>{a}: {c}棵"
-                },
-                xAxis : [
-                    {   
-                        name:"行政区域",
-                        nameTextStyle:{"fontSize":"14","color":"#808080"},
-                        type : 'category',
-                        boundaryGap : false,
-                        data : ['城北街道','城南街道','南口','沙河','南邵','崔村','阳坊','流村','马池口','十三陵','长陵','北七家'],
-                        axisLabel:{
-                            textStyle: {
-                                color: '#808080'
-                            }
-                        },
-                        axisLine:{
-                            lineStyle:{
-                                color: '#969696',
-                                width: 1,
-                                type: 'solid'
-                            }  
-                        },
-                        axisTick:{show:false}
-                    }
-                ],
-                yAxis : [
-                    {   
-                        name : '树木(棵)',
-                        nameTextStyle:{"fontSize":"14","color":"#808080"},
-                        type : 'value',
-                        axisLine:{
-                            lineStyle:{
-                                color: '#969696',
-                                width: 1,
-                                type: 'solid'
-                            }  
-                        }
-                        
-                    }
-                ],
-                series : [
-                    {
-                        name:'树木总数',
-                        type:'line',
-                        itemStyle: {
-                            normal: {
-                                color:"#547543",
-                                lineStyle: {
-                                    shadowColor : 'rgba(0,0,0,0.4)'
-                                }
-                            }
-                        },
-                        data:[20,100,123,400,90,70,230,600,500,400,200,300,445]
-                    }
-                ]
-            };
-            var pieOption = {
-                tooltip : {
-                    trigger: 'item',
-                    formatter: "{b}<br/>{a}: {c}棵"
-                },
-                series : [
-                    {
-                        name:'树木总数',
-                        type:'pie',
-                        radius : '55%',
-                        center: ['50%', '50%'],
-                        selectedOffset:20,
-                        data:[
-                        //['城北街道','','','','','','','','','','','']
-                        //[20,100,123,400,90,70,230,600,500,400,200,300,445]
-                            {value:20, name:'城北街道'},
-                            {value:100, name:'城南街道'},
-                            {value:123, name:'南口'},
-                            {value:400, name:'沙河'},
-                            {value:90, name:'南邵'},
-                            {value:70, name:'崔村'},
-                            {value:230, name:'阳坊'},
-                            {value:600, name:'流村'},
-                            {value:500, name:'马池口'},
-                            {value:400, name:'十三陵'},
-                            {value:200, name:'长陵'},
-                            {value:300, name:'北七家'}
-                        ]
-                    }
-                ]
-            };
-            var bdStyle;
-            var loadDom;
-            var comBox=$(".commonbox");
-            comBox.find(".tj-map-type").find("li").click(function (){
-                if($(this).hasClass("active")) return false;
-                $(this).addClass("active").siblings().removeClass("active");
-                loadChart(loadDom);
-            });
-            
-            var typename=localStorage.getItem("chartType") || getQueryString("type") || "gsdj"; 
+            //var barOption,lineOption,pieOption;
+            var barOption,lineOption,pieOption;
             var typeObject={
                 "gsdj":{
                     "setClass":"tj-tit-gsdj",
@@ -212,10 +45,217 @@ $(function (){
                     "nextName":"gsdj"
                 }
             };
+            //var myChart = ec.init(document.getElementById('tjmap'));
+            //type 1:古树等级 2：古树长势 3：古树比例 4： 区域分布
+            function init(typename){
+                var typeIndex=1; 
+                switch(typename){
+                    case "gsdj":
+                        typeIndex=1;
+                        break;
+                    case "gszs":
+                        typeIndex=2;
+                        break;
+                    case "szbl":
+                        typeIndex=3;
+                        break;
+                    case "qyfb":
+                        typeIndex=4;
+                        break;
+                }
+                
+                $.ajax({
+                    type:"get",
+                    url:"/inf/getTreeStatistics",
+                    data:{type:typeIndex},
+                    dataType:"json",
+                    error:function (error){
+                        var errormsg='获取统计信息出错，暂时无法向您展示统计数据'
+                        dialogMessage("统计信息",errormsg);
+                    },
+                    success:function (data){
+                        if(data.status==200){
+                            localStorage.setItem("chartType",typename);
+                            var list=data.data;
+                            var barData=[];
+                            var barValue=[];
+                            var pieData=[];
+                            for(var i=0;i<list.length;i++){
+                                barData.push(list[i].key);
+                                barValue.push(list[i].value);
+                                var pieObj={value:list[i].value, name:list[i].key},;
+                                pieData.push(pieObj);
+                            }
+                            barOption={
+                                tooltip : {
+                                    trigger: 'axis'
+                                    //,formatter: "{b}<br/>{a}: {c}棵"
+                                },
+                                xAxis : [
+                                    {
+                                        //name:"行政区域",
+                                        nameTextStyle:{"fontSize":"14","color":"#808080"},
+                                        data : barData,                        
+                                        axisLabel:{
+                                            textStyle: {
+                                                color: '#808080'
+                                            }
+                                        },
+                                        axisLine:{
+                                            lineStyle:{
+                                                color: '#969696',
+                                                width: 1,
+                                                type: 'solid'
+                                            }  
+                                        },
+                                        axisTick:{show:false}
+                                    }
+                                ],
+                                yAxis : [
+                                    {
+                                        //name : '树木(棵)',
+                                        nameTextStyle:{"fontSize":"14","color":"#808080"},
+                                        axisLabel:{
+                                            textStyle: {
+                                                color: '#808080'
+                                            }
+                                        },
+                                        axisLine:{
+                                            lineStyle:{
+                                                color: '#969696',
+                                                width: 1,
+                                                type: 'solid'
+                                            }  
+                                        }
+                                    }
+                                ],
+                                series : [
+                                    {
+                                       // name:'总数',
+                                        type:'bar',
+                                        barWidth:20, 
+                                        itemStyle: { 
+                                            normal: {
+                                                color: function(params) {
+                                                    var colorList = ['#91a86f','#7fa86f','#6fa870','#6fa88d','#6fa8a6','#7c9fb2','#8699b9','#a0a2c8','#b9a0c8','#ca9dba','#d4a9ac','#d5c096','#c5cc8c','#9ebc80','#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83','#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3','#91a86f','#7fa86f','#6fa870','#6fa88d','#6fa8a6','#7c9fb2','#8699b9','#a0a2c8','#b9a0c8','#ca9dba','#d4a9ac','#d5c096','#c5cc8c','#9ebc80','#c23531','#2f4554', '#61a0a8', '#d48265', '#91c7ae','#749f83','#ca8622', '#bda29a','#6e7074', '#546570', '#c4ccd3'];
+                                                    return colorList[params.dataIndex]
+                                                },
+
+                                                label : {
+                                                    show: true, 
+                                                    position: 'top',
+                                                    textStyle: {
+                                                        color: '#808080'
+                                                    }
+                                                }
+                                            }
+                                        },
+                                        data:barValue
+                                    }
+                                ]
+
+                            };
+                            lineOption={
+                                tooltip : {
+                                    trigger: 'axis'
+                                    //,formatter: "{b}<br/>{a}: {c}棵"
+                                },
+                                xAxis : [
+                                    {   
+                                        //name:"行政区域",
+                                        nameTextStyle:{"fontSize":"14","color":"#808080"},
+                                        type : 'category',
+                                        boundaryGap : false,
+                                        data : barData,
+                                        axisLabel:{
+                                            textStyle: {
+                                                color: '#808080'
+                                            }
+                                        },
+                                        axisLine:{
+                                            lineStyle:{
+                                                color: '#969696',
+                                                width: 1,
+                                                type: 'solid'
+                                            }  
+                                        },
+                                        axisTick:{show:false}
+                                    }
+                                ],
+                                yAxis : [
+                                    {   
+                                        //name : '树木(棵)',
+                                        nameTextStyle:{"fontSize":"14","color":"#808080"},
+                                        type : 'value',
+                                        axisLine:{
+                                            lineStyle:{
+                                                color: '#969696',
+                                                width: 1,
+                                                type: 'solid'
+                                            }  
+                                        }
+                                        
+                                    }
+                                ],
+                                series : [
+                                    {
+                                        //name:'树木总数',
+                                        type:'line',
+                                        itemStyle: {
+                                            normal: {
+                                                color:"#547543",
+                                                lineStyle: {
+                                                    shadowColor : 'rgba(0,0,0,0.4)'
+                                                }
+                                            }
+                                        },
+                                        data:barValue
+                                    }
+                                ]
+                            };
+                            pieOption = {
+                                tooltip : {
+                                    trigger: 'item'
+                                    //,formatter: "{b}<br/>{a}: {c}棵"
+                                },
+                                series : [
+                                    {
+                                        //name:'树木总数',
+                                        type:'pie',
+                                        radius : '55%',
+                                        center: ['50%', '50%'],
+                                        selectedOffset:20,
+                                        data:pieData
+                                    }
+                                ]
+                            };
+                            
+                            setTongjiData(typeObject[typename]);
+                        }else{
+                            var errormsg=data.error || '获取统计信息出错，暂时无法向您展示统计数据'
+                            dialogMessage("统计信息",errormsg);
+                        }
+                    }   
+                });
+            }
+            var chartName=localStorage.getItem("chartType") || getQueryString("type") || "gsdj";
+            init(chartName);
+            
+            var bdStyle;
+            var loadDom;
+            var comBox=$(".commonbox");
+            comBox.find(".tj-map-type").find("li").click(function (){
+                if($(this).hasClass("active")) return false;
+                $(this).addClass("active").siblings().removeClass("active");
+                loadChart(loadDom);
+            });
+            
+
+            
             comBox.find(".tj-arrow").find("div").click(function (){
                 var name=$(this).attr("name");
-                localStorage.setItem("chartType",name);
-                setTongjiData(typeObject[name]);
+                
+                init(name);
             });
             function setTongjiData(obj){
                 var setClass=obj.setClass;
@@ -253,7 +293,7 @@ $(function (){
                 myChart.setOption(option);
                 
             }
-            setTongjiData(typeObject[typename]);
+            
         }
     );
 });
